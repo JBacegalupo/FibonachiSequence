22cf8471b3290ef0100135153222b93f80465f73
JBacegalupo <J.BodgerBacegalupo2687@student.leedsbeckett.ac.uk>
2023-09-28 10:20:18 +0100

    just a test from a local machine

--- fibbonachi.py
+++ fibbonachi.py
@@ -3,11 +3,11 @@ CurrentNum = 1
 PreviousNum = 0
 temp = 0
 counter = 1
-# asks the user what fibonachi number they would like to work out and convert it to an integer
-print("please enter the fibonnaci number you would like to find")
+# asks the user what Fibonacci number they would like to work out and convert it to an integer
+print("please enter the Fibonacci number you would like to find")
 AmountOfTimes = input()
 AmountOfTimes = int(AmountOfTimes)
-# a while loop to work out the the next number in the fibonachi sequence
+# a while loop to work out the next number in the Fibonacci sequence
 while counter <= AmountOfTimes:
  temp = PreviousNum
  PreviousNum = CurrentNum
@@ -16,5 +16,5 @@ while counter <= AmountOfTimes:
  # breaks the loop when the desired number is found
  if counter >= AmountOfTimes:
   break
-# tells the user the value of the desired fibonachi number
-print("the " + str(AmountOfTimes) + " Fibbonachi number is " + str(CurrentNum))
+# tells the user the value of the desired Fibonacci number
+print("the " + str(AmountOfTimes) + " Fibonacci number is " + str(CurrentNum))
